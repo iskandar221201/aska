9b38b1f359748e5bc3a9f88272a2100e10b33d2f
iskandar221201 <85160476+iskandar221201@users.noreply.github.com>
2026-06-10 13:09:36 +0700

    balik lg

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -1,5 +1,5 @@
-import avatar from '../assets/images/avatar.jpg';
-import hero from '../assets/images/hero.jpg';
+// import avatar from '../assets/images/avatar.jpg';
+// import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
